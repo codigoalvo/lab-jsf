0f696952d8b1f3ef4dd68358c9750761824e2ba3
codigoalvo <codigoalvo@gmail.com>
2015-07-14 14:53:24 -0300

    fixes

--- src/main/java/codigoalvo/modelo/entidades/Usuario.java
+++ src/main/java/codigoalvo/modelo/entidades/Usuario.java
@@ -2,21 +2,23 @@ package codigoalvo.modelo.entidades;
 
 import java.io.Serializable;
 import java.util.Date;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Lob;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
+@Table(indexes = {@Index(name="usuario_login_unique", columnList="login", unique=true)})
 public class Usuario implements Serializable {
 
     private static final long serialVersionUID = 4860641136563274996L;
@@ -28,7 +30,6 @@ public class Usuario implements Serializable {
 
     @NotNull
     @Length(min = 3, max = 50)
-    @Column(length = 50, nullable = false, unique = true)
     private String login;
 
     @NotNull
---
+++ src/main/resources/META-INF/sql/dados-iniciais.sql
@@ -0,0 +1,2 @@
+insert into usuario (id, login, senha, nome, email, tipo) values (1, 'admin','c6zZpZchMLdQZsgllaH64w==', 'Administrador', 'admin@email.com', 'ADMIN');
+alter sequence SEQ_GRUPO_ID restart with 2;
