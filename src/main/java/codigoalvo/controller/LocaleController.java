49278ba65f8dd49d20dcc240562733a285293bde
Cassio <Cassio@Z97D-CASSIO-PC>
2015-08-11 23:04:48 -0300

    * Refactor do menu * favicon * Log no LocaleController

--- src/main/java/codigoalvo/controller/LocaleController.java
+++ src/main/java/codigoalvo/controller/LocaleController.java
@@ -14,30 +14,35 @@ import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 public class LocaleController extends SpringBeanAutowiringSupport implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = Logger.getLogger(LocaleController.class);
 
     private Locale currentLocale = new Locale("pt", "BR");
 
     public LocaleController() {
-	Logger.getLogger(LocaleController.class).debug("####################  construct  ####################");
+	LOGGER.debug("####################  construct  ####################");
     }
 
     public void englishLocale() {
+	LOGGER.debug("englishLocale");
 	UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
 	currentLocale = Locale.US;
 	viewRoot.setLocale(currentLocale);
     }
 
     public void portugueseLocale() {
+	LOGGER.debug("portugueseLocale");
 	UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
 	currentLocale = new Locale("pt", "BR");
 	viewRoot.setLocale(currentLocale);
     }
 
     public Locale getCurrentLocale() {
+	LOGGER.debug("getCurrentLocale");
 	return currentLocale;
     }
 
     public void setCurrentLocale(Locale currentLocale) {
+	LOGGER.debug("setCurrentLocale");
 	this.currentLocale = currentLocale;
     }
 
